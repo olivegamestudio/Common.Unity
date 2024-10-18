155cc96d937d00dc67611d8eb21e34dddfb65e8b
Simon Jefferies <simon@olivegame.studio>
2024-10-18 21:28:53 +0100

    Add common scripts.

---
+++ Runtime/ZoomCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ZoomCamera : MonoBehaviour
+{
+    public float Start;
+
+    public float End;
+
+    public float Duration;
+
+    float _currentTime;
+
+    void Update()
+    {
+        gameObject.GetComponent<Camera>().orthographicSize = Mathf.Lerp(Start, End, _currentTime / Duration);
+        _currentTime += Time.deltaTime;
+        if(_currentTime > Duration)
+        {
+            _currentTime = End;
+            gameObject.GetComponent<Camera>().orthographicSize = Mathf.Lerp(Start, End, _currentTime / Duration);
+            enabled = false;
+        }
+    }
+}
---
+++ Tests/Test.cs
@@ -0,0 +1,23 @@
+/*using NUnit.Framework;
+using UnityEngine.Assertions;
+using UnityEngine;
+
+namespace OliveGameStudio.Pilgrimage.Tests
+{
+    public class QuestProgressTests
+    {
+        [Test]
+        public void MyValue_ShouldBeZero_WhenInitialized()
+        {
+            // Arrange
+            var myRuntimeClass = new GameObject().AddComponent<QuestProgress>();
+
+            // Act
+            //int initialValue = myRuntimeClass.myValue;
+
+            // Assert
+            //Assert.AreEqual(0, initialValue);
+        }
+    }
+}
+*/
